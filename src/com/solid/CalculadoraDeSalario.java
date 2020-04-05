1263985a3586448f328003b1e09fb8daabaf3dc9
Yuri Almeida <yurialves2@gmail.com>
2020-04-05 02:03:56 -0300

    melhorano calculadora

--- src/com/solid/CalculadoraDeSalario.java
+++ src/com/solid/CalculadoraDeSalario.java
@@ -5,14 +5,9 @@ import static com.solid.Cargo.*;
 public class CalculadoraDeSalario {
 
     public double calcula(Funcionario funcionario) {
-        if(DESENVOLVEDOR.equals(funcionario.getCargo())) {
-            return new DezOuQuinzePorCento().calcular(funcionario);
-        }
-
-        if(DBA.equals(funcionario.getCargo()) || TESTER.equals(funcionario.getCargo())) {
-            return new QuinzeOuVinteECincoPorCento().calcular(funcionario);
-        }
-
-        throw new RuntimeException("funcionario invalido");
+        return funcionario
+                .getCargo()
+                .getRegraDeCalculo()
+                .calcular(funcionario);
     }
 }
